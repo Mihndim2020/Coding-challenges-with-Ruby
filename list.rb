fd83f1b122862c4aa48373708627558218d15c68
Mihndim <jelsieprisca2016@gmail.com>
2021-05-11 08:35:25 -0400

    Fix linter errors

--- list.rb
+++ list.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
+# This is initialize a node
 class Node
   attr_accessor :value, :next_node
-  
+
   def initialize(value, next_node = nil)
-	  @value = value
+    @value = value
     @next_node = next_node
   end
 end
 
+# This is to initilize a linked list
 class LinkedList
-  #setup head and tail
+  # setup head and tail
   def initilize
     @head = nil
     @tail = nil
   end
 
   def add(number)
-    #create a new node
+    # create a new node
     this_node = Node.new(number)
-  if @head.nil?
-    @head = this_node
-    return
-  end
+    if @head.nil?
+      @head = this_node
+      return
+    end
     current = @head
-  until current.next_node.nil?
-    current = current.next_node
-  end
+    current = current.next_node until current.next_node.nil?
     current.next_node = this_node
   end
-  
+
   def get(index)
-    current=@head
+    current = @head
     index.times do
-      current=current.next_node
+      current = current.next_node
     end
-    return current.value
+    current.value
   end
 end
